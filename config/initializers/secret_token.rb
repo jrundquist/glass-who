5292582c71ef3fcc210d92054e1d854153da33b5
James Rundquist <james.k.rundquist@gmail.com>
2013-06-06 01:49:06 -0400

    Fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassWho::Application.config.secret_token = 'c0b89f21be14b57e62e5b7c992a9011a6ca399a7c356787f9d27f4add9164b2b88b070ce545ef66d93d55701ea8880b69d7baaa1eab3e79ea2cfb1c5564a1f00'
